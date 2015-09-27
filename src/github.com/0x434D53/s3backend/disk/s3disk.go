438fbf34d8d8b9db7ef4d948d0d6994308d33590
0x434D53 <christoph.seufert@gmail.com>
2015-09-28 00:38:49 +0200

    ...

---
+++ src/github.com/0x434D53/s3backend/disk/s3disk.go
@@ -0,0 +1,138 @@
+package s3disk
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"sync"
+
+	"github.com/0x434D53/s3server/common"
+)
+
+type Options struct {
+	BasePath  string
+	CacheSize uint64
+}
+
+type Disk struct {
+	sync.RWMutex
+	Options
+}
+
+func (d *Disk) getBucketPath(bucketName string, auth string) string {
+	return filepath.Join(d.BasePath, auth, bucketName)
+}
+
+func (d *Disk) getFilePath(bucketName string, objectName string, auth string) string {
+	path := filepath.Join(d.BasePath, auth, bucketName, objectName)
+
+	return path
+}
+
+func exists(path string) (bool, error) {
+	_, err := os.Stat(path)
+
+	if err != nil {
+		if os.IsNotExist(err) {
+			return false, nil
+		} else {
+			return false, err
+		}
+	}
+
+	return true, nil
+}
+
+func existsBool(path string) bool {
+	b, _ := exists(path)
+
+	return b
+}
+
+func (d *Disk) GetService(auth string) (*common.ListAllMyBucketsResult, error) {
+	p := d.BasePath
+
+	f, err := os.Open(p)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer f.Close()
+
+	fis, err := f.Readdir(0)
+
+	var filtered []os.FileInfo
+
+	for _, fi := range fis {
+		if fi.IsDir() {
+			filtered = append(filtered, fi)
+		}
+
+		fmt.Println(fi.Name())
+	}
+
+	res := &common.ListAllMyBucketsResult{Buckets: make([]*common.Bucket, 0)}
+
+	for _, f := range filtered {
+		name := f.Name()
+		ctime := GetCTime(f)
+
+		res.Buckets = append(res.Buckets, &common.Bucket{Name: name, CreationDate: ctime})
+	}
+
+	return res, nil
+}
+
+func (d *Disk) DeleteBucket(bucketName string, auth string) error {
+	path := d.getBucketPath(bucketName, auth)
+
+	if !existsBool(path) {
+		return common.ErrNotFound
+	}
+
+	return os.Remove(path)
+}
+
+func (d *Disk) GetBucketObjects(bucketName string, auth string) ([]string, error) {
+	return nil, fmt.Errorf("Not implemented")
+}
+
+func (d *Disk) HeadBucket(bucketName string, auth string) error {
+	return fmt.Errorf("Not implemented")
+}
+
+func (d *Disk) PutBucket(bucketName string, auth string) error {
+	return fmt.Errorf("Not implemented")
+}
+
+func (d *Disk) DeleteObject(bucketName string, objectName string, auth string) error {
+	return fmt.Errorf("Not implemented")
+}
+
+func (d *Disk) GetObject(bucketName string, objectName string, auth string) ([]byte, error) {
+	return nil, fmt.Errorf("Not implemented")
+}
+
+func (d *Disk) HeadObject(bucketName string, objectName string, auth string) error {
+	return fmt.Errorf("Not implemented")
+}
+
+func (d *Disk) PutObject(bucketName string, objectName string, data []byte, auth string) error {
+	return fmt.Errorf("Not implemented")
+}
+
+func (d *Disk) PutObjectCopy(bucketName string, objectName string, targetBucket string, targetObject string, auth string) error {
+	return fmt.Errorf("Not implemented")
+}
+
+func (d *Disk) PostObject(bucketName string, objectName string, data []byte, auth string) error {
+	return fmt.Errorf("Not implemented")
+}
+
+func main() {
+	var be common.S3Backend
+
+	be = &Disk{}
+	fmt.Println(be)
+}
---
+++ src/github.com/0x434D53/s3backend/inMemory/s3inmemory.go
@@ -0,0 +1,165 @@
+package s3backend
+
+import "sync"
+
+type object struct {
+	name     string
+	contents []byte
+}
+
+type bucket struct {
+	objects map[string]*object
+	sync.Mutex
+}
+
+type S3InMemory struct {
+	buckets map[string]*bucket
+	sync.Mutex
+}
+
+func NewS3InMemory() S3Backend {
+	s3 := &S3InMemory{
+		buckets: make(map[string]*bucket, 0),
+	}
+
+	return s3
+}
+
+func (s3 *S3InMemory) PostObject(bucketName string, objectName string, data []byte, auth string) error {
+	s3.Lock()
+	defer s3.Unlock()
+
+	b, ok := s3.buckets[bucketName]
+
+	if !ok {
+		return ErrNotFound
+	}
+
+	b.objects[objectName] = &object{name: objectName, contents: data}
+
+	return nil
+}
+
+func (s3 *S3InMemory) PutObjectCopy(bucketName string, objectName string, targetBucketName string, targetObjectName string, auth string) error {
+	s3.Lock()
+	defer s3.Unlock()
+
+	b, err := s3.GetObject(bucketName, objectName, auth)
+
+	if err != nil {
+		return err
+	}
+
+	err = s3.PutObject(targetBucketName, targetObjectName, b, auth)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s3 *S3InMemory) PutObject(bucketName string, objectName string, data []byte, auth string) error {
+	s3.Lock()
+	defer s3.Unlock()
+
+	b, ok := s3.buckets[bucketName]
+	if !ok {
+		return ErrNotFound
+	}
+
+	b.objects[objectName] = &object{name: objectName, contents: data}
+
+	return nil
+}
+
+func (s3 *S3InMemory) DeleteBucket(bucketName string, auth string) error {
+	s3.Lock()
+	defer s3.Unlock()
+
+	delete(s3.buckets, bucketName)
+
+	return nil
+}
+
+func (s3 *S3InMemory) PutBucket(bucketName string, auth string) error {
+	s3.Lock()
+	defer s3.Unlock()
+
+	if _, ok := s3.buckets[bucketName]; !ok {
+		s3.buckets[bucketName] = &bucket{objects: make(map[string]*object, 0)}
+		return nil
+	}
+
+	return ErrAlreadyExists
+}
+
+func (s3 *S3InMemory) GetBucketObjects(bucketName string, auth string) ([]string, error) {
+	b, ok := s3.buckets[bucketName]
+
+	if !ok {
+		return nil, ErrNotFound
+	}
+
+	b.Lock()
+	defer b.Unlock()
+
+	ret := make([]string, 0, len(b.objects))
+
+	for k, _ := range b.objects {
+		ret = append(ret, k)
+	}
+
+	return ret, nil
+}
+
+func (s3 *S3InMemory) HeadBucket(bucket string, auth string) error {
+	s3.Lock()
+	defer s3.Unlock()
+
+	if _, ok := s3.buckets[bucket]; !ok {
+		return ErrNotFound
+	}
+
+	return nil
+}
+
+func (s3 *S3InMemory) HeadObject(bucket string, object string, auth string) error {
+	s3.Lock()
+	defer s3.Unlock()
+
+	if b, ok := s3.buckets[bucket]; !ok {
+		return ErrNotFound
+	} else {
+		if _, ok := b.objects[object]; !ok {
+			return ErrNotFound
+		}
+	}
+
+	return nil
+}
+
+func (s3 *S3InMemory) DeleteObject(bucket string, object string, auth string) error {
+	s3.Lock()
+	defer s3.Unlock()
+
+	return nil
+}
+
+func (s3 *S3InMemory) GetObject(bucket string, object string, auth string) ([]byte, error) {
+	s3.Lock()
+	defer s3.Unlock()
+
+	b, ok := s3.buckets[bucket]
+
+	if !ok {
+		return nil, ErrNotFound
+	}
+
+	o, ok := b.objects[object]
+
+	if !ok {
+		return nil, ErrNotFound
+	}
+	return o.contents, nil
+}
